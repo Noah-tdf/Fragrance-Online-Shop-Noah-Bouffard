4131f161fe4db6aceafb633797e53568e78befcc
Noah Bouffard <noahbouff@hotmail.com>
2025-10-14 16:49:42 -0400

    yes

--- src/main/java/org/example/termproject_bouffard/PresentationLayer/CustomerController.java
+++ src/main/java/org/example/termproject_bouffard/PresentationLayer/CustomerController.java
@@ -15,25 +15,25 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    // GET all customers
+
     @GetMapping
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
-    // GET customer by ID
+
     @GetMapping("/{id}")
     public Customer getCustomerById(@PathVariable Long id) {
         return customerService.getCustomerById(id);
     }
 
-    // POST a new customer
+
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
     }
 
-    // DELETE a customer
+
     @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable Long id) {
         customerService.deleteCustomer(id);
